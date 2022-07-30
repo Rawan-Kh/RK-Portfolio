56b05dd0d6f1b2a51beb5c6ee3c4147e74180459
rawan khalid <rawanKHsalama@gmail.com>
2022-07-30 09:00:47 +0200

    assets modification

--- src/portfolio.js
+++ src/portfolio.js
@@ -1,7 +1,9 @@
-import img1 from './assets/forkify.png'
-import img2 from './assets/mapty.png'
-import img3 from './assets/Pig-Game.png'
-import img4 from './assets/omnifood.png'
+import img1 from './assets/omnifood.png'
+import img2 from './assets/Pig-Game.png'
+import img3 from './assets/bankist.png'
+import img4 from './assets/bankist-website.png'
+import img5 from './assets/CountryApi.png'
+import img6 from './assets/Gueesing-game.png'
 
 const header = {
   homepage: 'https://',
@@ -10,13 +12,13 @@ const header = {
 
 const about = {
   name: 'Rawan Khaled',
-  role: 'Fullstack Developer',
+  role: 'Software Engineer',
   description:
-    ' Maiores minima consectetur Maiores minima consectetur Maiores minima consectetur.',
-  resume: 'https://example.com',
+    ' A software engineer as well as a machine learning enthusiat',
+  resume: 'https://resume.io/r/a451A2Xle',
   social: {
-    linkedin: 'https://linkedin.com',
-    github: 'https://github.com',
+    linkedin: 'https://www.linkedin.com/in/rawan-salama-1692021b6',
+    github: 'https://github.com/Rawan-Kh',
   },
 }
 
@@ -26,55 +28,54 @@ const projects = [
     img:img1,
     name: 'OmniFood website',
     description:
-      'Amet sed a nulla ipsa officia et esse aliquam',
+      'A healthy food app website design',
     stack: ['Html', 'Css', 'Javascript'],
     sourceCode: 'https://github.com/Rawan-Kh/Kalbonyan-Elmarsos/tree/main/2-udemy/1-Html-Css/projects/OmniFood-Project',
-    livePreview: 'https://rawan-kh.github.io/Kalbonyan-Elmarsos/2-udemy/1-Html-Css/projects/OmniFood-Project/https://github.com',
+    livePreview: 'https://rawan-kh.github.io/Kalbonyan-Elmarsos/2-udemy/1-Html-Css/projects/OmniFood-Project/',
   },
   {
     img:img2,
-    name: 'Mapty app',
-    description:
-      'Amet sed a nulla ipsa officia et esse aliquam',
-    stack: ['SASS', 'TypeScript', 'React'],
-    sourceCode: 'https://github.com',
-    livePreview: 'https://github.com',
-  },
-  {
-    img:img4,
-    name: 'Omnifood website',
-    description:
-      'Amet sed a nulla ipsa officia et esse aliquam',
-    stack: ['SASS', 'TypeScript', 'React'],
-    sourceCode: 'https://github.com',
-    livePreview: 'https://github.com',
-  },
-  {
-    img:img3,
     name: 'Pig-Game',
     description:
-      'Amet sed a nulla ipsa officia et esse aliquam',
-    stack: ['SASS', 'TypeScript', 'React'],
-    sourceCode: 'https://github.com',
-    livePreview: 'https://github.com',
+      'A simple javascript game',
+    stack: ['Html', 'Css', 'Javascript'],
+    sourceCode: 'https://github.com/Rawan-Kh/Kalbonyan-Elmarsos/tree/main/2-udemy/2-Js/projects/PigGame/',
+    livePreview: 'https://rawan-kh.github.io/Kalbonyan-Elmarsos/2-udemy/2-Js/projects/PigGame/',
   },
     {
-    img:img2,
-    name: 'Mapty app',
+    img:img3,
+    name: 'Bankist App',
     description:
-      'Amet sed a nulla ipsa officia et esse aliquam',
-    stack: ['SASS', 'TypeScript', 'React'],
-    sourceCode: 'https://github.com',
-    livePreview: 'https://github.com',
+      'A bank simulation app',
+    stack: ['Html', 'Css', 'Js'],
+    sourceCode: 'https://github.com/Rawan-Kh/Kalbonyan-Elmarsos/tree/main/2-udemy/2-Js/projects/BankistApp',
+    livePreview: 'https://rawan-kh.github.io/Kalbonyan-Elmarsos/2-udemy/2-Js/projects/BankistApp/',
   },
   {
     img:img4,
-    name: 'Omnifood website',
+    name: 'Bankist website',
     description:
-      'Amet sed a nulla ipsa officia et esse aliquam',
-    stack: ['SASS', 'TypeScript', 'React'],
-    sourceCode: 'https://github.com',
-    livePreview: 'https://github.com',
+      'A website design with amazing functionalities',
+    stack: ['Html', 'css', 'Javascript'],
+    sourceCode: 'https://github.com/Rawan-Kh/Kalbonyan-Elmarsos/tree/main/2-udemy/2-Js/projects/BankistWebsite',
+    livePreview: 'https://https://rawan-kh.github.io/Kalbonyan-Elmarsos/2-udemy/2-Js/projects/BankistWebsite/',
+  },{
+    img:img5,
+    name: 'Countries n Neighbours',
+    description:
+      'A fetching countries information and the neighbouring Api',
+    stack: ['Html', 'Js', 'FetchApi'],
+    sourceCode: 'https://github.com/Rawan-Kh/Kalbonyan-Elmarsos/tree/main/2-udemy/2-Js/projects/CountryApi',
+    livePreview: 'https://https://rawan-kh.github.io/Kalbonyan-Elmarsos/2-udemy/2-Js/projects/CountryApi/',
+  },
+  {
+    img:img6,
+    name: 'Guessing Game',
+    description:
+      'A simple js guessing game',
+    stack: ['Html', 'css', 'Javascript'],
+    sourceCode: 'https://github.com/Rawan-Kh/Kalbonyan-Elmarsos/tree/main/2-udemy/2-Js/projects/GuessingGame',
+    livePreview: 'https://https://rawan-kh.github.io/Kalbonyan-Elmarsos/2-udemy/2-Js/projects/GuessingGame/',
   },
 ]
 
